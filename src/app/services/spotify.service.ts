95623196bcf82484c51c35801fdbe628c086af34
Joan Blanco <joanblanco1906@gmail.com>
2022-09-04 15:23:24 -0400

    refactorizando la llamada al servicio spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,23 +9,32 @@ import { map } from 'rxjs/operators';
 export class SpotifyService {
   constructor(private http: HttpClient) {}
 
-  getNewReleases() {
-    const url = 'https://api.spotify.com/v1/browse/new-releases?limit=20';
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
+
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQB7S6snWRU3aFq-zAFV98oyyoRogQ6EhlEFZtaS7nKj5JK5XUl4D1u3APQviVqv_tT6xZLCFN6KzwML-8slU3k7EOMza0KD8DgJM794WdqoNEpAdww',
+        'Bearer BQDRB0iJsS6NoB2Vb3fGWXUG_KymViq7UYF59lq-TVjX9KLuBo2ZiyiKTV0a34zt3pYciRAiF5oNoa6QBhJYbDEiItWlAV5NsTAh39s_0CstMTA3nGk',
     });
-    return this.http.get(url, { headers }).pipe(map((data: any) => data['albums'].items));
+
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases() {
+    return this.getQuery('browse/new-releases?limit=20')
+    .pipe(
+      map(
+        (data: any) => data['albums'].items
+      )
+    );
   }
 
   getArtist(termino: string) {
-    const url = `https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15&offset=5`;
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQB7S6snWRU3aFq-zAFV98oyyoRogQ6EhlEFZtaS7nKj5JK5XUl4D1u3APQviVqv_tT6xZLCFN6KzwML-8slU3k7EOMza0KD8DgJM794WdqoNEpAdww'
-    });
-    return this.http.get(url, { headers }).pipe(
-      map((data: any) => data['artists'].items)
+    return this.getQuery('search?q=${termino}&type=artist&limit=15&offset=5')
+    .pipe(
+      map(
+        (data: any) => data['artists'].items
+      )
     );
   }
 }
