66d01ca44815a11f6ac51a7309fc19578375b249
Joan Blanco <joanblanco1906@gmail.com>
2022-09-04 13:50:53 -0400

    consumiendo servicio en el componente del search

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,27 +5,23 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   // paises: any[] = [];
   newReleases: any[] = [];
 
-  constructor(private http: HttpClient,
-    private spotify: SpotifyService) {
+  constructor(private http: HttpClient, private spotify: SpotifyService) {
     // http.get('https://restcountries.com/v2/lang/es').subscribe((pais: any) => {
     //   this.paises = pais;
     //   console.log(this.paises)
     // })
 
-    this.spotify.getNewReleases().subscribe((data: any) => {
-      this.newReleases = data.albums.items;
-      console.log(this.newReleases);
-    })
+    this.spotify
+      .getNewReleases()
+      .subscribe((data) => (this.newReleases = data));
+      console.log(this.newReleases)
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  newArtists: any[] = [];
 
-  constructor() { }
+  constructor(private spotify: SpotifyService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  search(termino: string) {
+    this.spotify
+      .getArtist(termino)
+      .subscribe(artista => this.newArtists = artista);
+      console.log(this.newArtists)
   }
-
 }
