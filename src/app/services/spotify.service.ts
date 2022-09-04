cc6bbcd0e7b56b668a2db341d77c385340dc3eaf
Joan Blanco <joanblanco1906@gmail.com>
2022-09-04 13:50:23 -0400

    add service for artists y add map

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,19 +1,31 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { map } from 'rxjs/operators';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SpotifyService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getNewReleases() {
     const url = 'https://api.spotify.com/v1/browse/new-releases?limit=20';
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCAqTMdrDBsKJq5smJ2Q36SrGJ0JRmRWKGyiC7a78LVyicqSgVWLrLTF_czcxAR7NA-TzEu0O9DPMOj-A9s6tjEyWpYcEfIqtf70cujj2foUeRvtic'
+      Authorization:
+        'Bearer BQB7S6snWRU3aFq-zAFV98oyyoRogQ6EhlEFZtaS7nKj5JK5XUl4D1u3APQviVqv_tT6xZLCFN6KzwML-8slU3k7EOMza0KD8DgJM794WdqoNEpAdww',
     });
+    return this.http.get(url, { headers }).pipe(map((data: any) => data['albums'].items));
+  }
 
-    return this.http.get(url, {headers});
+  getArtist(termino: string) {
+    const url = `https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15&offset=5`;
+    const headers = new HttpHeaders({
+      Authorization:
+        'Bearer BQB7S6snWRU3aFq-zAFV98oyyoRogQ6EhlEFZtaS7nKj5JK5XUl4D1u3APQviVqv_tT6xZLCFN6KzwML-8slU3k7EOMza0KD8DgJM794WdqoNEpAdww'
+    });
+    return this.http.get(url, { headers }).pipe(
+      map((data: any) => data['artists'].items)
+    );
   }
 }
